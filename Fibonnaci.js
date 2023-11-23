eb51621167a3ac82ace4e40373d5da74175da3c5
raissalim <142915471+raissalim@users.noreply.github.com>
2023-11-23 07:44:48 -0300

    Update Fibonnaci.js

--- Fibonnaci.js
+++ Fibonnaci.js
@@ -1,1 +1,19 @@
 
+function generateFibonacci() {
+      let n = parseInt(document.getElementById('numberInput').value);
+      let fibonacciSeries = [];
+
+      if (n === 0) {
+        fibonacciSeries.push(0);
+      } else if (n === 1) {
+        fibonacciSeries.push(0, 1);
+      } else {
+        fibonacciSeries.push(0, 1);
+        for (let i = 2; i < n; i++) {
+          let newTerm = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+          fibonacciSeries.push(newTerm);
+        }
+      }
+
+      document.getElementById('fibonacciSeries').innerText = 'Série de Fibonacci: ' + fibonacciSeries.join(', ');
+    }
