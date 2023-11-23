b2e4671dfff1f74a90a14ab4e828cc4095ec77de
raissalim <142915471+raissalim@users.noreply.github.com>
2023-11-23 07:16:12 -0300

    Create Niver.js

---
+++ Niver.js
@@ -0,0 +1,56 @@
+var mes = prompt("Digite o número do mês do seu aniversário:");
+
+    switch (parseInt(mes)) {
+        case 1:
+            alert("Janeiro");
+            document.write("<h1>Janeiro</h1>");
+            break;
+        case 2:
+            alert("Fevereiro");
+            document.write("<h1>Fevereiro</h1>");
+            break;
+        case 3:
+            alert("Março");
+            document.write("<h1>Março</h1>");
+            break;
+        case 4:
+            alert("Abril");
+            document.write("<h1>Abril</h1>");
+            break;
+        case 5:
+            alert("Maio");
+            document.write("<h1>Maio</h1>");
+            break;
+        case 6:
+            alert("Junho");
+            document.write("<h1>Junho</h1>");
+            break;
+        case 7:
+            alert("Julho");
+            document.write("<h1>Julho</h1>");
+            break;
+        case 8:
+            alert("Agosto");
+            document.write("<h1>Agosto</h1>");
+            break;
+        case 9:
+            alert("Setembro");
+            document.write("<h1>Setembro</h1>");
+            break;
+        case 10:
+            alert("Outubro");
+            document.write("<h1>Outubro</h1>");
+            break;
+        case 11:
+            alert("Novembro");
+            document.write("<h1>Novembro</h1>");
+            break;
+        case 12:
+            alert("Dezembro");
+            document.write("<h1>Dezembro</h1>");
+            break;
+        default:
+            alert("Mês inválido");
+            document.write("<h1>Mês inválido</h1>");
+            break;
+    }
