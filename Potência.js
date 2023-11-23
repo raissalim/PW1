18bea554eb5f3b8d2858a7a679843f15806e123d
raissalim <142915471+raissalim@users.noreply.github.com>
2023-11-23 07:28:47 -0300

    Update Potência.js

--- Potência.js
+++ Potência.js
@@ -1,1 +1,7 @@
-
+function calcularPotencia() {
+  
+     let b = parseInt(document.getElementById("baseDaPotencia").value);
+      let ex = parseInt(document.getElementById("expoenteDaPotencia").value);
+      let resultadoPotencia = Math.pow(b, ex);// metodo oara calcular de forma simples o edpoente
+  document.getElementById("resultado").innerText = "O resultado da potência é: " + resultadoPotencia;
+}
